735f80fda9dfd93610b5c99bfd4d28c7c12e3f07
Rafał Rzepecki <divided.mind@gmail.com>
2009-07-15 07:43:59 +0200

    Initial commit. Just a template.

---
+++ moviesorter.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write(template.render("moviesorter.html", {}))
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
