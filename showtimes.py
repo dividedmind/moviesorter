28c7b23adf4d670ff0b5a65972d6e42a4b75a12f
Rafał Rzepecki <divided.mind@gmail.com>
2009-07-15 09:04:57 +0200

    showtimes get loaded

--- showtimes.py
+++ showtimes.py
@@ -4,7 +4,11 @@ import urllib, re
 from logging import info, debug
 
 BASE = 'http://www.google.com/movies?hl=en&sort=3&near='
-TITLE_RE = re.compile(r'<tr valign=top>.*?<a href="(/movies\?hl=en[^"]*?&mid=[^"]*?)"><b dir=ltr>(.*?)</b>.*?</tr>')
+TITLE_RE = re.compile(r'mid=([^"]*?)"><b dir=ltr>(.*?)</b>(.*?)<tr><td>&nbsp;</td></tr>')
+CINEMA_RE = re.compile(r'tid=([0-9a-f]*)"><b dir=ltr>(.*?)</b></a><br>.*?<br>(.*?)</font></td>')
+
+def movielink(city, mid):
+    return "http://www.google.com/movies?hl=en&near=" + urllib.quote(city) + "&mid=" + mid
 
 def find(city):
     url = BASE + urllib.quote(city)
@@ -14,7 +18,20 @@ def find(city):
     movies = [ ]
     it = TITLE_RE.finditer(showtimes_page)
     for i in it:
-        movies.append({'link': 'http://www.google.com' + i.group(1), 'title': i.group(2)})
+        mid = i.group(1)
+        title = i.group(2)
+        rest = i.group(3)
+
+        jt = CINEMA_RE.finditer(rest)
+        showtimes = []
+        for j in jt:
+            tid = j.group(1)
+            cinema = j.group(2)
+            times = j.group(3).split('&nbsp; ')
+            for t in times:
+                showtimes.append({'time': t})
+
+        movies.append({'link': movielink(city, mid), 'title': title, 'showtimes': showtimes})
 
     debug("movies: " + unicode(movies))
 
