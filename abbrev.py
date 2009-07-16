a7cd294c1af8251c6f8ba799692d7808827f3798
Rafał Rzepecki <divided.mind@gmail.com>
2009-07-17 00:30:51 +0200

    Nicer abbrevations.

--- abbrev.py
+++ abbrev.py
@@ -6,4 +6,4 @@ register = webapp.template.create_template_register()
 
 @register.filter
 def abbrev(name):
-    return ''.join([word[0].upper() for word in name.split()])
+    return ''.join([word[0] for word in name.split()])
