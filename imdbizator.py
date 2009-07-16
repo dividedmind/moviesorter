4b52ecaef7b276433b547ca0c017c1ba9e4e357f
Rafał Rzepecki <divided.mind@gmail.com>
2009-07-16 04:19:31 +0200

    Fetch imdb data.

--- imdbizator.py
+++ imdbizator.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
 
-import imdb
+import re
+from logging import debug
 
-def imdbize(showtimes):
-    return showtimes
+from imdb import IMDb
+
+from memoize import gaecache
+
+ia = IMDb()
+
+def massage_title(original_title):
+    # hacks, heuristics &c go here
+    return re.sub(r'^(.*), The', r'The \1', original_title)
+
+@gaecache()
+def best_guess(title):
+    title = massage_title(title)
+    im = ia.search_movie(title)
+    if im:
+        return im[0]
+    else:
+        return None
+
+def imdbize(movies):
+    for m in movies:
+        im = best_guess(m['title'])
+        debug("imdb for " + m['title'] + ": " + unicode(im))
+    return movies
