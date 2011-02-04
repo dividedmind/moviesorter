236fb2cedc7ec0fb197ebdedcedc52a0311dfc3c
Rafał Rzepecki <divided.mind@gmail.com>
2011-02-04 18:43:57 +0100

    Handle geonames not responding.

--- geonames.py
+++ geonames.py
@@ -12,22 +12,25 @@ from memoize import gaecache
 def find(place):
     BASE_URL = "http://ws.geonames.org/searchJSON?maxRows=1&q="
     while True:
-        url = BASE_URL + urllib.quote(place.encode('utf-8'))
-        response = unicode(urllib.urlopen(url).read(), 'utf-8')
-        result = simplejson.loads(response)
-        # TODO: not cache if no 'totalResultsCount'
-        if 'totalResultsCount' in result and result['totalResultsCount'] > 0:
-            return result['geonames'][0]
-        else:
-            # try to fix the placename
-            # (heuristics, hacks, etc. go here)
-
-            # Irish counties
-            newname = place.replace("Co.", "County")
-            if newname != place:
-                place = newname
+        try:
+            url = BASE_URL + urllib.quote(place.encode('utf-8'))
+            response = unicode(urllib.urlopen(url).read(), 'utf-8')
+            result = simplejson.loads(response)
+            # TODO: not cache if no 'totalResultsCount'
+            if 'totalResultsCount' in result and result['totalResultsCount'] > 0:
+                return result['geonames'][0]
             else:
-                return None
+                # try to fix the placename
+                # (heuristics, hacks, etc. go here)
+
+                # Irish counties
+                newname = place.replace("Co.", "County")
+                if newname != place:
+                    place = newname
+                else:
+                    return None
+        except DownloadError:
+            return None
 
 @gaecache()
 def timezone(place):
